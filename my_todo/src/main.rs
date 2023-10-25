c87b94cbf195d1e3f25a8ff5efd46823a98361bc
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-10-26 07:59:38 +0900

    app TaskRepository

--- my_todo/src/main.rs
+++ my_todo/src/main.rs
@@ -7,6 +7,49 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::net::SocketAddr;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum RepositoryError {
+    #[error("NotFound, id is {0}")]
+    NotFound(i32),
+}
+
+pub trait TaskRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+    fn create(&self, payload: CreateTask) -> Task;
+    fn find(&self, id: i32) -> Option<Task>;
+    fn all(&self) -> Vec<Task>;
+    fn update(&self, id: i32, payload: UpdateTask) -> anyhow::Result<Task>;
+    fn delete(&self, id: i32) -> anyhow::Result<()>;
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct Task {
+    id: i32,
+    text: String,
+    completed: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct CreateTask {
+    text: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct UpdateTask {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+impl Task {
+    pub fn new(id: i32, text: String) -> Self {
+        Self {
+            id,
+            text,
+            completed: false,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
