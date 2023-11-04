f68f24abc7c1dd07603ba18144b5a539027cb1cc
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-11-04 10:22:02 +0900

    app label モックテスト追加

--- my_todo/src/repositories/label.rs
+++ my_todo/src/repositories/label.rs
@@ -10,7 +10,7 @@ pub trait LabelRepository {
     async fn delete(&self, id: i32) -> anyhow::Result<()>;
 }
 
-#[derive(sqlx::FromRow)]
+#[derive(sqlx::FromRow, Clone, PartialEq, Eq, Debug)]
 pub struct Label {
     pub id: i32,
     pub name: String,
@@ -124,5 +124,103 @@ mod test {
             .delete(label.id)
             .await
             .expect("[delete] returned Err");
+    }
+}
+
+#[cfg(test)]
+pub mod test_utils {
+    use std::{
+        collections::HashMap,
+        sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard},
+    };
+
+    use super::*;
+
+    impl Label {
+        pub fn new(id: i32, name: String) -> Self {
+            Self { id, name }
+        }
+    }
+
+    type LabelData = HashMap<i32, Label>;
+
+    pub struct LabelRepositoryForMemory {
+        store: Arc<RwLock<LabelData>>,
+    }
+    impl LabelRepositoryForMemory {
+        pub fn new() -> Self {
+            LabelRepositoryForMemory {
+                store: Arc::default(),
+            }
+        }
+
+        fn write_store_ref(&self) -> RwLockWriteGuard<LabelData> {
+            self.store.write().unwrap()
+        }
+        fn read_store_ref(&self) -> RwLockReadGuard<LabelData> {
+            self.store.read().unwrap()
+        }
+    }
+
+    #[async_trait]
+    impl LabelRepository for LabelRepositoryForMemory {
+        async fn create(&self, name: String) -> anyhow::Result<Label> {
+            let mut store = self.write_store_ref();
+            let id = (store.len() + 1) as i32;
+            let label = Label::new(id, name);
+            store.insert(id, label.clone());
+            Ok(label)
+        }
+        async fn all(&self) -> anyhow::Result<Vec<Label>> {
+            let store = self.read_store_ref();
+            Ok(Vec::from_iter(store.values().map(|label| label.clone())))
+        }
+        async fn delete(&self, id: i32) -> anyhow::Result<()> {
+            let mut store = self.write_store_ref();
+            store.remove(&id).ok_or(RepositoryError::NotFound(id))?;
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn label_crud_scenario() {
+        let name = "label name".to_string();
+        let id = 1;
+        let expected = Label::new(id, name.clone());
+        let repository = LabelRepositoryForMemory::new();
+
+        // create
+        let label = repository.create(name).await.expect("failed create label");
+        assert_eq!(expected, label);
+
+        // find
+        /*
+        let label = repository.find(label.id).await.unwrap();
+        assert_eq!(expected, label);
+        */
+
+        // all
+        let labels = repository.all().await.expect("failed get all label");
+        assert_eq!(vec![expected], labels);
+
+        // update
+        /*
+        let name = "update label name".to_string();
+        let label = repository
+            .update(
+                1,
+                UpdateLabel {
+                    id,
+                    name: name.clone(),
+                },
+            )
+            .await
+            .expect("failed update label.");
+        assert_eq!(Label { id, name }, label);
+        */
+
+        // delete
+        let res = repository.delete(id).await;
+        assert!(res.is_ok());
     }
 }
