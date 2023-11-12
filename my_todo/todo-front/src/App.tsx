af81732deaab0159704ee67f959a05312f5b778a
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-11-12 10:09:02 +0900

    app label sidebar

--- my_todo/todo-front/src/App.tsx
+++ my_todo/todo-front/src/App.tsx
@@ -2,13 +2,17 @@ import { FC, useEffect, useState } from 'react';
 import 'modern-css-reset';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Box, Stack, Typography } from '@mui/material';
-import { NewTaskPayload, Task } from './types/task';
+import { Label, NewLabelPayload, NewTaskPayload, Task } from './types/task';
 import TaskList from './components/TaskList.tsx';
 import TaskForm from './components/TaskForm.tsx';
 import { addTaskItem, deleteTaskItem, getTaskItems, updateTaskItem } from './lib/api/task.ts';
+import SideNav from './components/SideNav.tsx';
+import { addLabelItem, deleteLabelItem, getLabelItems } from './lib/api/label.ts';
 
 const TodoApp: FC = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [labels, setLabels] = useState<Label[]>([]);
+    const [filterLabelId, setFilterLabelId] = useState<number | null>(null);
 
     const onSubmit = async (payload: NewTaskPayload) => {
         if (!payload.text) {
@@ -35,10 +39,34 @@ const TodoApp: FC = () => {
         setTasks(tasks);
     };
 
+    // filtering tasks by selected label
+    const onSelectLabel = (label: Label | null) => {
+        setFilterLabelId(label?.id ?? null);
+    };
+
+    const onSubmitNewLabel = async (newLabel: NewLabelPayload) => {
+        if (!labels.some((label) => label.name === onSubmitNewLabel.name)) {
+            const res = await addLabelItem(newLabel);
+            setLabels([...labels, res]);
+        }
+    };
+
+    const onDeleteLabel = async (id: number) => {
+        await deleteLabelItem(id);
+        setLabels((prev) => prev.filter((label) => label.id !== id));
+
+    };
+
+    const tasksToDisplay = filterLabelId
+        ? tasks.filter((task) => task.labels.some((label) => label.id === filterLabelId))
+        : tasks;
+
     useEffect(() => {
         ; (async () => {
             const tasks = await getTaskItems();
             setTasks(tasks);
+            const labelResponse = await getLabelItems();
+            setLabels(labelResponse);
         })();
     }, []);
 
@@ -62,6 +90,25 @@ const TodoApp: FC = () => {
             </Box>
             <Box
                 sx={{
+                    backgroundColor: 'white',
+                    borderRight: '1px solid gray',
+                    position: 'fixed',
+                    height: 'calc(100%-80px)',
+                    width: 200,
+                    zIndex: 2,
+                    left: 0,
+                }}
+            >
+                <SideNav
+                    labels={labels}
+                    onSelectLabel={onSelectLabel}
+                    filterLabelId={filterLabelId}
+                    onSubmitNewLabel={onSubmitNewLabel}
+                    onDeleteLabel={onDeleteLabel}
+                />
+            </Box>
+            <Box
+                sx={{
                     display: 'flex',
                     justifyContent: 'center',
                     p: 5,
@@ -71,7 +118,7 @@ const TodoApp: FC = () => {
                 <Box maxWidth={700} width="100%">
                     <Stack spacing={5}>
                         <TaskForm onSubmit={onSubmit} />
-                        <TaskList tasks={tasks} onUpdate={onUpdate} onDelete={onDelete} />
+                        <TaskList tasks={tasksToDisplay} onUpdate={onUpdate} onDelete={onDelete} />
                     </Stack>
                 </Box>
             </Box>
--- my_todo/todo-front/src/types/task.d.ts
+++ my_todo/todo-front/src/types/task.d.ts
@@ -2,8 +2,18 @@ export type Task = {
     id: number;
     text: string;
     completed: boolean;
+    labels: Label[];
 };
 
 export type NewTaskPayload = {
     text: string;
+};
+
+export type Label = {
+    id: number;
+    name: string;
+};
+
+export type NewLabelPayload = {
+    name: string;
 };
